ef8720384f867f336dfe9e25353de9e2f9c38a51
adebureaux <adebureaux@gmail.com>
2023-05-03 22:24:51 +0200

    Working simple searchbar

--- src/pages/SearchBar.tsx
+++ src/pages/SearchBar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 interface Advise {
 	unique_name: string;
+	local_name: string;
 }
 
 interface Autocomplete {
@@ -13,21 +14,42 @@ const SearchBar: React.FunctionComponent = () => {
 	const [query, setQuery] = useState<string>("");
 	const [advise, setAdvise] = useState<Advise[]>([]);
 	const [autocomplete, setAutocomplete] = useState<Autocomplete[]>([]);
+	const [popular, setPopular] = useState<Boolean>(true);
+	const [focus, setFocus] = useState<Boolean>(false);
+	const adviseDepth : number = 5; // Change this variable and set the number of advises result you want when calling the API
+
+	const chooseDisplay = (str : string) => {
+		if (str.length > 1) {
+			setPopular(false);
+			getPopularCities(adviseDepth);
+		}
+		else {
+			getAutocomplete(str);
+			setPopular(true);
+		}
+	};
+
+	const reset = () => {
+		setPopular(false);
+		setFocus(false);
+	}
 
 	const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault(); // Avoid the refresh on the page when pressing enter or click on search
-		// console.log(`user current search input: "${query}"`);
-		getAutocomplete(query);
+		console.log(`user current search input: "${query}"`);
+		chooseDisplay(query);
 	};
-	
+
 	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setQuery(event.target.value);
-		// console.log(`user submit input: "${event.target.value}"`);
+		chooseDisplay(event.target.value);
+		console.log(`user submit input: "${event.target.value}"`);
 	};
 
 	const handleFocus = (event: React.FocusEvent<HTMLInputElement>) => {
-		getPopularCities(5);
-		// console.log(`user focused "${event.target.value}"`);
+		chooseDisplay(event.target.value);
+		setFocus(true);
+		console.log(`user focused "${event.target.value}"`);
 	};
 
 	const getPopularCities = async (n : number) => {
@@ -41,10 +63,10 @@ const SearchBar: React.FunctionComponent = () => {
 		setAutocomplete(data);
 		console.log('cities based on the search: ', data);
 	};
-	
+
 	return (
 		<div className="searchbar m-24">
-			<div className="container">
+
 			<form onSubmit={handleFormSubmit}>
 				<input
 					className="rounded-full"
@@ -53,13 +75,33 @@ const SearchBar: React.FunctionComponent = () => {
 					value={query}
 					onChange={handleInputChange}
 					onFocus={handleFocus}
+					onBlur={reset}
 				/>
 					<button type="submit">
 						<img className="rounded-full" src="search.png" />
 					</button>
 			</form>
-			</div>
-			</div>
+
+			<ul className="bg-white m-2 rounded-xl">
+				{focus && <div className="p-4">
+					{!popular && autocomplete.map(
+						(data: Autocomplete) => (
+							<li key={data.unique_name}>
+								{data.local_name}
+							</li>
+						)
+					)}
+					{popular && advise.map(
+						(data: Advise) => (
+							<li key={data.unique_name}>
+								{data.local_name}
+							</li>
+						)
+					)}
+				</div>}
+			</ul>
+
+		</div>
 	);
 };
 
